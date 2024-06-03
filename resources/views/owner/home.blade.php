45080d91798b51338dd1f50c14c6ccf68815a7ee
moazzamjanjua <moazzamjanjua92@gmail.com>
2024-06-03 16:18:33 +0500

    moazzam changes

--- resources/views/owner/home.blade.php
+++ resources/views/owner/home.blade.php
@@ -161,36 +161,44 @@
     <div class="dashboard">
         <div class="sidebar">
             <h2>Owner Information</h2>
-          <form action="{{ route('owner.update', ['id' => $owner->id]) }}" method="POST" enctype="multipart/form-data" id="update-form">
-    @csrf
-    @method('PUT')
-    <div class="avatar-container">
-        <img src="{{ $owner->avatar ?? '/frontend/img/home/avatar.jpg' }}" alt="Avatar" class="avatar" id="owner-avatar">
-        <button type="button" class="edit-icon" onclick="document.getElementById('avatar-input').click();">✎</button>
-        <input type="file" name="avatar" id="avatar-input" class="file-input" accept="image/*" onchange="updateAvatar(event)">
-    </div>
-    <ul class="profile-info">
-        <li><span>Name:</span> <span id="owner-name">{{ $owner->name ?? 'N/A' }}</span></li>
-        <li>
-            <span>Phone Number:</span>
-            <span id="owner-phone"><input type="text" name="phone" value="{{ $owner->phone ?? 'N/A' }}"></span>
-        </li>
-        <li><span>Email:</span> <span id="owner-email">{{ $owner->email ?? 'N/A' }}</span></li>
-        <li>
-            <span>Country:</span>
-            <span id="owner-country"><input type="text" name="country" value="{{ $owner->country ?? 'N/A' }}"></span>
-        </li>
-        <li>
-            <span>City:</span>
-            <span id="owner-city"><input type="text" name="city" value="{{ $owner->city ?? 'N/A' }}"></span>
-        </li>
-        <li>
-            <span>Permanent Address:</span>
-            <span id="owner-address"><input type="text" name="address" value="{{ $owner->address ?? 'N/A' }}"></span>
-        </li>
-    </ul>
-    <button type="submit" class="action-button">Update</button>
-</form>
+            <form action="{{ route('owner.update', ['id' => $owner->id]) }}" method="POST" enctype="multipart/form-data"
+                id="update-form">
+                @csrf
+                @method('PUT')
+                <div class="avatar-container">
+                    <img src="{{ $owner->owner_image ?? '/frontend/img/home/avatar.jpg' }}" alt="Avatar" class="avatar"
+                        id="owner_image">
+                    <button type="button" class="edit-icon"
+                        onclick="document.getElementById('avatar-input').click();">✎</button>
+                    <input type="file" name="owner_image" id="avatar-input" class="file-input" accept="image/*"
+                        onchange="updateAvatar(event)">
+                </div>
+                <ul class="profile-info">
+                <li><span>Name:</span> <span id="owner-email">{{ $owner->name ?? 'N/A' }}</span></li>
+                <li>
+                        <span>Phone Number:</span>
+                        <span id="owner-phone"><input type="text" name="phone"
+                                value="{{ $owner->phone ?? 'N/A' }}"></span>
+                    </li>
+                    <li><span>Email:</span> <span id="owner-email">{{ $owner->email ?? 'N/A' }}</span></li>
+                    <li>
+                        <span>Country:</span>
+                        <span id="owner-country"><input type="text" name="country"
+                                value="{{ $owner->country ?? 'N/A' }}"></span>
+                    </li>
+                    <li>
+                        <span>City:</span>
+                        <span id="owner-city"><input type="text" name="city" value="{{ $owner->city ?? 'N/A' }}"></span>
+                    </li>
+                    <li>
+                        <span>Permanent Address:</span>
+                        <span id="owner-address"><input type="text" name="permanent_address"
+                                value="{{ $owner->permanent_address ?? 'N/A' }}"></span>
+                    </li>
+                </ul>
+                <button type="submit" class="action-button">Update</button>
+            </form>
+
 
 
             <a href="" class="logout-button">Logout</a>
@@ -207,7 +215,7 @@
             const reader = new FileReader();
 
             reader.onload = function (e) {
-                document.getElementById('owner-avatar').src = e.target.result;
+                document.getElementById('owner_image').src = e.target.result;
             };
 
             reader.readAsDataURL(file);
