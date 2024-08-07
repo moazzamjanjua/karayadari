ffdc7ff68e58e43319ec6df526dac32434792496
Jawad Ahmad <jawadahmad6588@gmail.com>
2024-08-07 11:51:05 +0500

    dg

--- resources/views/owner/room-form.blade.php
+++ resources/views/owner/room-form.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Details Form</title>
+    <title>Room Details Form</title>
     <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
     <style>
         body {
